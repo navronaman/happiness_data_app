c867e231f9228b3bec18b6cadb5e87f5f46285f7
navronaman <namanmaulikshah@gmail.com>
2023-01-19 15:44:43 -0500

    Increased formatting of graphs

--- functions.py
+++ functions.py
@@ -8,8 +8,14 @@ df = pd.read_csv("happy.csv")
 def get_data(valuex, valuey, op1, op2):
     x_axis = df[valuex]
     y_axis = df[valuey]
+    color = df["happiness"]
+    hover = df["country"]
 
-    figure = px.scatter(x=x_axis, y=y_axis, labels={"x":op1, "y": op2})
+    figure = px.scatter(x=x_axis, y=y_axis,
+                        labels={"x": op1, "y": op2,
+                                "hover_data_0": "Country",
+                                "color": "Happiness"},
+                        color=color, hover_data=[hover])
 
     return x_axis, y_axis, figure
 
